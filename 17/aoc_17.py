52b02056308b225cf539b6672cc93a142220ed44
errapSknowit <erraps@SE-PF4DLMLC>
2024-01-11 12:32:15 +0100

    Day 17 solutions

--- 17/aoc_17.py
+++ 17/aoc_17.py
@@ -7,57 +7,51 @@ RIGHT = (0, 1)
 DOWN = (1, 0)
 LEFT = (0, -1)
 
-def is_valid(node, consecutive_steps, direction, previous_direction):
-    if consecutive_steps == 3 and direction == previous_direction:
-        return False
-    elif 0 <= node[0] < rows and 0 <= node[1] < cols:
-        return True
-    return False
-    
 
-def cheapest_path(grid, src, end):
-    priority_queue = [(grid[src], 1, 0, src)]
-    visited = set()
-
-    distances = {vertex: float('infinity') for vertex in np.ndindex(grid.shape)}
-    distances[src] = grid[src]
-    consecutive_steps_count = {vertex: 1 for vertex in np.ndindex(grid.shape)}
+def cheapest_path(grid, src, end, part1):
+    priority_queue = [(0, -1, 0, src)]
+    visited = {}
+    paths = {vertex: [] for vertex in np.ndindex(grid.shape)}
 
     while priority_queue:
-        current_distance, current_consecutive_steps, previous_direction, current_vertex = heapq.heappop(priority_queue)
+        current_distance, current_consecutive_steps, current_direction, current_vertex = heapq.heappop(priority_queue)
 
-        if current_vertex in visited:
+        if (current_consecutive_steps, current_direction, current_vertex) in visited:
             continue
 
-        visited.add(current_vertex)
+        visited[(current_consecutive_steps, current_direction, current_vertex)] = current_distance
 
-        for neighbor in [UP, RIGHT, DOWN, LEFT]:
-            dx, dy = neighbor
-            new_vertex = current_vertex[0] + dx, current_vertex[1] + dy
-            
-            if is_valid(new_vertex, current_consecutive_steps, neighbor, previous_direction):
-                weight = grid[new_vertex]
-                distance = current_distance + weight
+        for i, direction in enumerate([UP, RIGHT, DOWN, LEFT]):
+            new_vertex = current_vertex[0] + direction[0], current_vertex[1] + direction[1]
+            new_consecutive_steps = (1 if i != current_direction else current_consecutive_steps + 1)
+            new_direction = i
+            if part1:
+                if 0 <= new_vertex[0] < rows and 0 <= new_vertex[1] < cols and new_consecutive_steps < 4 and (new_direction + 2) % 4 != current_direction:
+                    weight = grid[new_vertex]
+                    distance = current_distance + weight
+                    paths[new_vertex] = paths[current_vertex] + [current_vertex]
+                    heapq.heappush(priority_queue, (distance, new_consecutive_steps, new_direction, new_vertex))
+            else:
+                if 0 <= new_vertex[0] < rows and 0 <= new_vertex[1] < cols and (new_consecutive_steps < 11 and (current_consecutive_steps == -1 or current_consecutive_steps >= 4 or i == current_direction )) and (new_direction + 2) % 4 != current_direction:
+                    weight = grid[new_vertex]
+                    distance = current_distance + weight
+                    paths[new_vertex] = paths[current_vertex] + [current_vertex]
+                    heapq.heappush(priority_queue, (distance, new_consecutive_steps, new_direction, new_vertex))
 
-                if distance < distances[new_vertex]:
-                    distances[new_vertex] = distance
+    shortest_path_distance = []
+    for (current_consecutive_steps, current_direction, current_vertex), dist in visited.items():
+        if current_vertex == end:
+            shortest_path_distance.append(dist)
 
-                    if neighbor == previous_direction or previous_direction == 0:
-                        consecutive_steps_count[new_vertex] = current_consecutive_steps + 1
-
-                    heapq.heappush(priority_queue, (distance, consecutive_steps_count[new_vertex], neighbor, new_vertex))
-                   
-    shortest_path_distance = distances[end]
-    
-    return shortest_path_distance
+    return min(shortest_path_distance) if shortest_path_distance else 0
 
 
-
-city = np.array([list(map(int, line.strip())) for line in open('17/test.txt').read().split('\n')])
+city = np.array([list(map(int, line.strip())) for line in open('17/input.txt').read().split('\n')])
 
 rows, cols = city.shape
 start = (0, 0)
 end = (rows - 1, cols - 1)
 
-r =cheapest_path(city, start, end)
-print(r)
+print('ANSWER PART A: ', cheapest_path(city, start, end, True))
+
+print('ANSWER PART B: ', cheapest_path(city, start, end, False))
